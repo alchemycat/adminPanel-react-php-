8cf3589a1993a1e363f327ce6ae2eb373e55445e
yo <colacat1927@gmail.com>
2022-09-15 16:55:04 +0300

    добавил резервные копии

--- app/src/components/choose-modal/choose-modal.js
+++ app/src/components/choose-modal/choose-modal.js
@@ -2,18 +2,37 @@ import React from "react";
 import UIkit from "uikit";
 
 const ChooseModal = ({ modal, target, data, redirect }) => {
-  const pageList = data.map((item) => {
-    return (
-      <li key={item}>
-        <a
-          className="uk-link-muted uk-modal-close"
-          href="#"
-          onClick={(e) => redirect(e, item)}>
-          {item}
-        </a>
-      </li>
-    );
+  const list = data.map((item) => {
+    if (item.time) {
+      return (
+        <li key={item.file}>
+          <a
+            className="uk-link-muted uk-modal-close"
+            href="#"
+            onClick={(e) => redirect(e, item.file)}>
+            Резервная копия от {item.time}
+          </a>
+        </li>
+      );
+    } else {
+      return (
+        <li key={item}>
+          <a
+            className="uk-link-muted uk-modal-close"
+            href="#"
+            onClick={(e) => redirect(e, item)}>
+            {item}
+          </a>
+        </li>
+      );
+    }
   });
+
+  let msg;
+
+  if (data.length < 1) {
+    msg = <div>Резервные копии не найдены</div>;
+  }
 
   return (
     <div
@@ -21,7 +40,8 @@ const ChooseModal = ({ modal, target, data, redirect }) => {
       uk-modal={modal.toString()}>
       <div className="uk-modal-dialog uk-modal-body">
         <h2 className="uk-modal-title">Открыть</h2>
-        <ul class="uk-list uk-list-divider">{pageList}</ul>
+        {msg}
+        <ul className="uk-list uk-list-divider">{list}</ul>
         <p className="uk-text-right">
           <button
             className="uk-button uk-button-default uk-modal-close"
